933533ab9b8a87a4513c0ea12dcc52a5c7d67559
Nicolay Gerold <nicolay.gerold@tum.de>
2024-12-08 16:53:05 +0100

    added homebrew .rb

--- setup.py
+++ setup.py
@@ -1,67 +1,40 @@
-import shlex
-from configparser import ConfigParser
+from setuptools import setup, find_packages
+import os
 
-import setuptools
-from pkg_resources import parse_version
+VERSION = "0.1.0"  # Matches pyproject.toml
 
-assert parse_version(setuptools.__version__)>=parse_version('36.2')
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
 
-# note: all settings are in settings.ini; edit there, not here
-config = ConfigParser(delimiters=['='])
-config.read('settings.ini', encoding='utf-8')
-cfg = config['DEFAULT']
-
-cfg_keys = 'version description keywords author author_email'.split()
-expected = cfg_keys + "lib_name user branch license status min_python audience language".split()
-for o in expected: assert o in cfg, "missing expected setting: {}".format(o)
-setup_cfg = {o:cfg[o] for o in cfg_keys}
-
-licenses = {
-    'apache2': ('Apache Software License 2.0','OSI Approved :: Apache Software License'),
-    'mit': ('MIT License', 'OSI Approved :: MIT License'),
-    'gpl2': ('GNU General Public License v2', 'OSI Approved :: GNU General Public License v2 (GPLv2)'),
-    'gpl3': ('GNU General Public License v3', 'OSI Approved :: GNU General Public License v3 (GPLv3)'),
-    'bsd3': ('BSD License', 'OSI Approved :: BSD License'),
-}
-statuses = [ '1 - Planning', '2 - Pre-Alpha', '3 - Alpha',
-    '4 - Beta', '5 - Production/Stable', '6 - Mature', '7 - Inactive' ]
-py_versions = '3.6 3.7 3.8 3.9 3.10 3.11 3.12'.split()
-
-requirements = shlex.split(cfg.get('requirements', ''))
-if cfg.get('pip_requirements'): requirements += shlex.split(cfg.get('pip_requirements', ''))
-min_python = cfg['min_python']
-lic = licenses.get(cfg['license'].lower(), (cfg['license'], None))
-dev_requirements = (cfg.get('dev_requirements') or '').split()
-
-package_data = dict()
-pkg_data = cfg.get('package_data', None)
-if pkg_data:
-    package_data[cfg['lib_name']] =  pkg_data.split() # split as multiple files might be listed
-# Add package data to setup_cfg for setuptools.setup(..., **setup_cfg)
-setup_cfg['package_data'] = package_data
-
-setuptools.setup(
-    name = cfg['lib_name'],
-    license = lic[0],
-    classifiers = [
-        'Development Status :: ' + statuses[int(cfg['status'])],
-        'Intended Audience :: ' + cfg['audience'].title(),
-        'Natural Language :: ' + cfg['language'].title(),
-    ] + ['Programming Language :: Python :: '+o for o in py_versions[py_versions.index(min_python):]] + (['License :: ' + lic[1] ] if lic[1] else []),
-    url = cfg['git_url'],
-    packages = setuptools.find_packages(),
-    include_package_data = True,
-    install_requires = requirements,
-    extras_require={ 'dev': dev_requirements },
-    dependency_links = cfg.get('dep_links','').split(),
-    python_requires  = '>=' + cfg['min_python'],
-    long_description = open('README.md', encoding='utf-8').read(),
-    long_description_content_type = 'text/markdown',
-    zip_safe = False,
-    entry_points = {
-        'console_scripts': cfg.get('console_scripts','').split(),
-        'nbdev': [f'{cfg.get("lib_path")}={cfg.get("lib_path")}._modidx:d']
+setup(
+    name="shell-sage",
+    description="ShellSage: AI-powered shell commands in your terminal (Fork from https://github.com/AnswerDotAI/shell_sage)",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Nicolay Gerold",
+    author_email="nicolay.gerold@gmail.com",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["ssage=shell_sage.core:app"]
     },
-    **setup_cfg)
-
-
+    install_requires=[
+        "aisuite>=0.1.6",
+        "anthropic>=0.40.0",
+        "openai>=1.57.0",
+        "rich>=13.9.4",
+        "typer>=0.15.1",
+    ],
+    extras_require={
+        "dev": [
+            "isort>=5.13.2",
+            "pytest",
+        ]
+    },
+    python_requires=">=3.12",
+)
---
+++ shell_sage.rb
@@ -0,0 +1,44 @@
+class ShellSage < Formula
+    include Language::Python::Virtualenv
+
+    desc "AI-powered shell commands in your terminal"
+    homepage "https://github.com/nicolaygerold/homebrew-shell-sage"
+    url "https://github.com/nicolaygerold/homebrew-shell-sage/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    license "Apache-2.0"
+
+    depends_on "python@3.12"
+
+    resource "aisuite" do
+      url "https://files.pythonhosted.org/packages/source/a/aisuite/aisuite-0.1.6.tar.gz"
+      sha256 "0937372a4aa369ad63fa2350cf58780fdad6914e651e97ca7f6192a517e933b7"
+    end
+
+    resource "anthropic" do
+      url "https://files.pythonhosted.org/packages/source/a/anthropic/anthropic-0.40.0.tar.gz"
+      sha256 "3efeca6d9e97813f93ed34322c6c7ea2279bf0824cd0aa71b59ce222665e2b87"
+    end
+
+    resource "openai" do
+      url "https://files.pythonhosted.org/packages/source/o/openai/openai-1.57.0.tar.gz"
+      sha256 "76f91971c4bdbd78380c9970581075e0337b5d497c2fbf7b5255078f4b31abf9"
+    end
+
+    resource "rich" do
+      url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.9.4.tar.gz"
+      sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
+    end
+
+    resource "typer" do
+      url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.15.1.tar.gz"
+      sha256 "a0588c0a7fa68a1978a069818657778f86abe6ff5ea6abf472f940a08bfe4f0a"
+    end
+
+    def install
+      virtualenv_install_with_resources
+    end
+
+    test do
+      assert_match "ShellSage", shell_output("#{bin}/ssage --help")
+    end
+  end
